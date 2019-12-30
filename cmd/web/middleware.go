0f40a446870d2b51bcc17e20b842eba8244bb872
sonja <stefan.gajic@ymail.com>
2019-12-30 22:29:53 +0100

    Added user authorization

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -34,3 +34,26 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (app *application) requireAuthentication(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !app.isAuthenticated(r) {
+			http.Redirect(w, r, "/user/login", 302)
+			return
+		}
+
+		w.Header().Add("Cache-Control", "no-store")
+		next.ServeHTTP(w, r)
+	})
+}
+
+// func noSurf(next http.Handler) http.Handler {
+// 	csrfHandler := nosurf.New(next)
+// 	csrfHandler.SetBaseCookie(http.Cookie{
+// 		HttpOnly: true,
+// 		Path:     "/",
+// 		Secure:   true,
+// 	})
+
+// 	return csrfHandler
+// }
